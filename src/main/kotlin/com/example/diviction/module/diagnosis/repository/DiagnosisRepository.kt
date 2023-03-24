4a960c0340286e38d25d4de0d3abc53ef8f7b072
WooJoongKim <rlandwnd555@naver.com>
2023-03-24 23:18:13 +0900

    remove : 삭제된 테이블에 대한 기능 삭제

--- src/main/kotlin/com/example/diviction/module/diagnosis/repository/DiagnosisRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.diviction.module.diagnosis.repository
-
-import com.example.diviction.module.diagnosis.entity.DiagnosisResult
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface DiagnosisRepository : JpaRepository<DiagnosisResult,Long> {
-}
