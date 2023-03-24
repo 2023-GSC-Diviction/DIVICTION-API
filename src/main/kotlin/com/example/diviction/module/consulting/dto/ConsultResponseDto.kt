9595a047cac409bf7567887987b124d3a69c8920
WooJoongKim <rlandwnd555@naver.com>
2023-03-24 23:21:00 +0900

    fix : DB 구조 마이그레이션에 따른 DTO 필드 수정

--- src/main/kotlin/com/example/diviction/module/consulting/dto/ConsultResponseDto.kt
+++ src/main/kotlin/com/example/diviction/module/consulting/dto/ConsultResponseDto.kt
@@ -4,8 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDate
 
 data class ConsultResponseDto(
-    val counselorId : Long,
-    val patientId : Long,
+    val matchingId: Long,
     val content : String,
     @field: JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     val date : LocalDate
