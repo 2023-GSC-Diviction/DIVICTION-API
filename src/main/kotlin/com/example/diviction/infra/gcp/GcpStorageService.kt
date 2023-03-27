5272dd2c1d4e566ff9c7b19540eb19cd5beb4730
Identityless <rlandwnd555@naver.com>
2023-03-27 19:28:51 +0900

    feat : 기존 파일 삭제 기능 추가

--- src/main/kotlin/com/example/diviction/infra/gcp/GcpStorageService.kt
+++ src/main/kotlin/com/example/diviction/infra/gcp/GcpStorageService.kt
@@ -30,4 +30,20 @@ class GcpStorageService(
     fun getFileEx(fileName: String): String {
         return fileName.substring(fileName.lastIndexOf(".") + 1).lowercase()
     }
+
+    fun deleteFileToGCS(
+        url: String
+    ) {
+        val fileName = getFileNameFromUrl(url)
+        //TODO: 나중에 적절한 예외를 던지는것으로 변경
+        val blob = storage.get(bucketName, "user-profile/${fileName}") ?: throw RuntimeException("해당 파일은 존재하지 않습니다.")
+
+        val precondition: Storage.BlobSourceOption = Storage.BlobSourceOption.generationMatch(blob.generation)
+
+        storage.delete(bucketName, "user-profile/${fileName}", precondition)
+    }
+
+    fun getFileNameFromUrl(url: String): String {
+        return url.substring(url.lastIndexOf("/") + 1)
+    }
 }
