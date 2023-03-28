4ccb22ae4c553db966ed98afcd0b843f3836b4dc
Identityless <rlandwnd555@naver.com>
2023-03-28 16:26:57 +0900

    fix : 스토리지 경로 url 중복되는 버그 수정

--- src/main/kotlin/com/example/diviction/infra/gcp/CommonCodes.kt
+++ src/main/kotlin/com/example/diviction/infra/gcp/CommonCodes.kt
@@ -1,8 +1,9 @@
 package com.example.diviction.infra.gcp
 
 object GCP_URLs{
-    val STORAGE_BASE_URL = "https://storage.googleapis.com/diviction/user-profile/"
-    val COUNSELOR_BASIC_IMG_URL = STORAGE_BASE_URL + "counselor.png" // 상담사 기본 프로필 사진
-    val PATIENT_BASIC_IMG_URL = STORAGE_BASE_URL + "patient.png"  // 환자 기본 프로필 사진
+    val STORAGE_BASE_URL = "https://storage.googleapis.com/diviction/"
     val USER_PROFILE_URL = STORAGE_BASE_URL + "user-profile/"
+    val COUNSELOR_BASIC_IMG_URL = USER_PROFILE_URL + "counselor.png" // 상담사 기본 프로필 사진
+    val PATIENT_BASIC_IMG_URL = USER_PROFILE_URL + "patient.png"  // 환자 기본 프로필 사진
+
 }
